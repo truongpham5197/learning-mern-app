c76ffdc1acb71b4ae2fe34593e0e7e2b73248db6
truongpham5197 <truongpn050197@gmail.com>
2023-01-26 11:53:19 +0700

    Implement CRUD api users

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  passwrod: {
+  password: {
     type: String,
     required: true,
   },
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/usersController");
+
+router
+  .route("/")
+  .get(userController.getAllusers)
+  .post(userController.createNewUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
